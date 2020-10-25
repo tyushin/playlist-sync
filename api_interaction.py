0d8453e68ca5479bae3d3a728f4f1c8d8a3744de
Tyushin Artem <artart998@yandex.ru>
2020-10-25 04:41:53 +0300

    Restructuring methods. Test searching.

--- api_interaction.py
+++ api_interaction.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, url_for
 import requests
+import webbrowser
 
 app = Flask(__name__)
 
@@ -9,12 +10,14 @@ DEEZER_APP_ID = "439942"
 DEEZER_APP_SECRET = "20aa27ced2d98642cb2a0007da7daf99"
 DEEZER_REDIRECT_URI = "http://127.0.0.1:5000/deezer/login"
 
+
 # Class for intermediate playlist model
 class IntermediatePlaylist:
     def __init__(self, playlist_id, title, tracks):
         self.playlist_id = playlist_id
         self.title = title
         self.tracks = tracks
+
 
 # Class for intermediate track model
 class IntermediateTrack:
@@ -23,6 +26,7 @@ class IntermediateTrack:
         self.title = title
         self.album_title = album_title
         self.artist_name = artist_name
+
 
 # Default path. Redirect user to authentication service.
 @app.route('/', methods=['GET'])
@@ -53,10 +57,25 @@ def deezer_login():
     global login_access_token
     login_access_token = response['access_token']
     # Redirect user to playlists URL
-    return redirect(url_for('get_playlists'))
+    return redirect(url_for('get_info'))
 
 
-@app.route('/playlists', methods=['GET'])
+# Get all tracks from playlist
+def get_tracklist(playlist_id):
+    # Get JSON object (dictionary) for list of playlist tracks
+    response = requests.get(f'https://api.deezer.com/playlist/{playlist_id}/tracks',
+                            {'access_token': login_access_token})
+    response = response.json()
+
+    tracklist = []
+    # Create IntermediateTrack model for each track in dictionary
+    for element in response['data']:
+        track = IntermediateTrack(element['id'], element['title'], element['album']['title'], element['artist']['name'])
+        tracklist.append(track)
+    return tracklist
+
+
+# Get user playlists
 def get_playlists():
     # Get JSON object (dictionary) for list of user playlists
     response = requests.get(f'https://api.deezer.com/user/me/playlists', {'access_token': login_access_token})
@@ -68,45 +87,52 @@ def get_playlists():
         playlist = IntermediatePlaylist(element['id'], element['title'], [])
         playlists.append(playlist)
 
-    # For each playlist get JSON object (dictionary) for list of playlist tracks
+    # Fill each playlist with tracks
     for playlist in playlists:
-        response = requests.get(f'https://api.deezer.com/playlist/{playlist.playlist_id}/tracks',
-                                {'access_token': login_access_token})
-        response = response.json()
-
-        tracklist = []
-        # Create IntermediateTrack model for each track in dictionary
-        for element in response['data']:
-            track = IntermediateTrack(element['id'], element['title'], element['album']['title'], element['artist']['name'])
-            tracklist.append(track)
         # Add list of tracks into IntermediatePlaylist model
-        playlist.tracks = tracklist
+        playlist.tracks = get_tracklist(playlist.playlist_id)
+
+    return playlists
+
+
+# Search track
+def search_track(track_info):
+    search_info = ''
+    for key, value in track_info.items():
+        search_info += f'{key}:\"{value}\" '
+
+    # Get JSON object (dictionary) for searching track
+    response = requests.get(f'https://api.deezer.com/search/?q={search_info}', {'access_token': login_access_token})
+    response = response.json()
+
+    return response
+
+
+@app.route('/info', methods=['GET'])
+def get_info():
+    playlists = get_playlists()
 
     # Create string to view result elements
     result = 'Playlists:\n'
     for playlist in playlists:
         result += f'Playlist title: {playlist.title}:\n'
         for track in playlist.tracks:
-            result += f'\tTitle: {track.title};\n'
-            result += f'\tAlbum title: {track.album_title};\n'
-            result += f'\tArtist name: {track.artist_name};\n'
+            result += '\t Track:\n'
+            result += f'\t\tTitle: {track.title};\n'
+            result += f'\t\tAlbum title: {track.album_title};\n'
+            result += f'\t\tArtist name: {track.artist_name};\n'
+    # TEST
+    print(result)
 
-    return result
+    # TEST for searching tracks
+    track_info = {'track': 'Our Story', 'artist': 'Mako'}
+    return search_track(track_info)
 
-
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-
-
-@app.route('/shutdown', methods=['POST'])
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
 
 # Add: search tracks, create playlist, add tracks into playlist, exception handler (if cannot do something).
 
+
 if __name__ == '__main__':
+    # Auto open web page
+    webbrowser.open_new_tab("http://127.0.0.1:5000/")
     app.run()
